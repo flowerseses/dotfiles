8949710a22599e29b8957c047a1649a868cc63b3
Iva Marika Dervisevic <ivan@deepinsight.io>
2024-08-20 13:14:17 +0200

    Initial arch/sway/zsh dotfiles

---
+++ sway/swaylock.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+args="--ignore-empty-password -S --effect-blur 10x5"
+
+lock() {
+	  touch ~/.swaylocked
+	  if ~/.local/bin/swaylock "$@"; then
+		    rm -f ~/.swaylocked
+	  fi
+}
+
+if [ "$1" = immediate ]; then
+	  lock $args &
+else
+	  lock $args --grace 20 --fade-in 10 &
+fi
---
+++ waybar/waybar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# Terminate already running bar instances
+killall -q waybar
+
+# Wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+# Launch main
+waybar -c ${HOME}/.config/waybar/config.json -s ${HOME}/.config/waybar/style.css
